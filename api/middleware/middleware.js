c69481787705000aeba24ecbf6ccdd42f10a5690
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-21 04:42:33 -0400

    all tests passing

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,65 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`${req.method} ${req.url} ${Date.now()}`)
+  next()
 }
 
-function validateUserId(req, res, next) {
+const validateUserId = (Users) => (req, res, next) => {
   // DO YOUR MAGIC
+  const id = req.params.id;
+  if (id) {
+    Users.getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user
+        next()
+      } else {
+        res.status(404).json({
+          message: "user not found"
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Server error"
+      })
+    })
+  } else {
+    next()
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  if (req.body == {}) {
+    res.status(400).json({
+      message: 'missing user data'
+    })
+  } else if (!req.body.name) {
+    res.status(400).json({
+      message: 'missing required name field'
+    })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  if (req.body == {}) {
+    res.status(400).json({
+      message: "missing post data"
+    })
+  } else if (!req.body.text) {
+    res.status(400).json({
+      message: "missing required text field"
+    })
+  } else {
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = { logger, validateUserId, validateUser, validatePost }
